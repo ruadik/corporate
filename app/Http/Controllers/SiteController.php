430e0ff88e0d9bd0d22264cdba68fc970a801328
Ali Ru <vagrant@homestead>
2019-06-11 08:50:01 +0000

    therd

--- app/Http/Controllers/SiteController.php
+++ app/Http/Controllers/SiteController.php
@@ -36,7 +36,7 @@ class SiteController extends Controller
         $navigation = view(env('THEME').'.navigation')->render();
         $this->vars = array_add($this->vars, 'navigation', $navigation);
 
-        return view($this->template)->with($this->varу);
+        return view($this->template)->with($this->vars);
 
     }
 
